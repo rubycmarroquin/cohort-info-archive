76d283bd4e17e9a9a7f749324114ed05a7000809
Yazmin <yazminmoniquetorres@gmail.com>
2023-06-05 16:26:56 -0700

    add comments for each solution

--- client/src/components/CodeThread.jsx
+++ client/src/components/CodeThread.jsx
@@ -21,9 +21,30 @@ const CodeThread = ({ title, date, link }) => {
   const solutionsList = solutions.map((solution) => (
     <SolutionsRow
       key={solution.id}
+      solutionId={solution.id}
       url={solution.url}
       name={solution.username}
     />
+  ));
+
+  const comments = [
+    {
+      id: 1,
+      username: "Yazmin",
+      description: "This is a really good challenge!",
+    },
+    {
+      id: 2,
+      username: "Pandora",
+      description: "is this the type of challenge we can expect in interviews?",
+    },
+  ];
+
+  // will need to make a get request to get all the general comments associated with the main thread
+  // create a commentsList using comments component (Ruby)
+
+  const commentsList = comments.map((comment) => (
+    <p key={comment.id}>{comment.description}</p>
   ));
 
   return (
@@ -35,6 +56,10 @@ const CodeThread = ({ title, date, link }) => {
       <p>Date: {date}</p>
       <a href={link}>Link to Code Challenge</a>
       {solutionsList}
+      <div className="comments-section">
+        <p>General Comments Section</p>
+        {commentsList}
+      </div>
       <button>Add Solution</button>
     </div>
   );
--- client/src/components/SolutionsRow.jsx
+++ client/src/components/SolutionsRow.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const SolutionsRow = ({ url, name }) => {
+const SolutionsRow = ({ url, name, solutionId }) => {
+  const comments = [
+    {
+      id: 1,
+      username: "Yazmin",
+      description: "why did you decided to use for instead of while loop?",
+    },
+    {
+      id: 2,
+      username: "Ruby",
+      description:
+        "can you explain why we needed to use the spread operator here?",
+    },
+  ];
+
   // will need to make a get requests to get all the comments associated with a specific solution
+  // create a commentsList using comments component (Ruby)
+
+  const commentsList = comments.map((comment) => (
+    <p key={comment.id}>{comment.description}</p>
+  ));
 
   return (
     <div className="solution-row" style={{ backgroundColor: "lightgray" }}>
@@ -12,8 +31,8 @@ const SolutionsRow = ({ url, name }) => {
         <p>{name}</p>
       </div>
       <div className="comments-section">
-        <h6>Comments:</h6>
-        <p>Comment 1</p>
+        <p>Comments:</p>
+        {commentsList}
       </div>
     </div>
   );
