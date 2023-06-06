55d0c27451bbb5dc64895b0591544004f110d445
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-06-05 23:20:16 -0700

    rearranged ordering of contents

--- client/src/components/SolutionsRow.jsx
+++ client/src/components/SolutionsRow.jsx
@@ -5,15 +5,13 @@ const SolutionsRow = ({ link, username, solutionId, title, description }) => {
   return (
     <div className="solution-row" style={{ backgroundColor: "lightgray" }}>
       <div className="header">
-        <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
-          <h3>
-            <a href={link}>{title}</a>
-          </h3>
-          <p style={{ fontWeight: "bold" }}>{username}</p>
-        </div>
+        <h4 style={{ fontWeight: "bold" }}>{username}</h4>
         <p>
           <strong>Description:</strong> {description}
         </p>
+        <h4>
+          <a href={link}>{title}</a>
+        </h4>
       </div>
       <div className="comments-section">
         <ListComments category={"solution"} id={solutionId} />
