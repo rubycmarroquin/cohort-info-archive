fde6cd7f04c4a534d91398055b168e761d153261
Yolis <yolisisabel28@gmail.com>
2023-06-06 00:31:13 -0700

    created basic format and modified data based off of post req from backend

---
+++ client/src/components/SolutionForm.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const SolutionForm = () => {
+  const [id, setId] = useState("");
+  const [username, setUsername] = useState("");
+  const [link, setLink] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/solutions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, username, link }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        // Handle success or display a success message
+      } else {
+        throw new Error("Request failed");
+      }
+    } catch (error) {
+      console.error(error);
+      // Handle error or display an error message
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        ID:
+        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Link:
+        <input
+          type="text"
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default SolutionForm;
