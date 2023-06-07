70f815a57beea1e41c37317fc18fe396d85c463f
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-06-06 17:15:36 -0700

    Added modal for solutions

--- client/src/components/SolutionForm.jsx
+++ client/src/components/SolutionForm.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 
-const SolutionForm = ({ id }) => {
+const SolutionForm = ({ handleClose, loadSolutions, id }) => {
   const [username, setUsername] = useState("");
   const [link, setLink] = useState("");
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("/api/solutions", {
+      const response = await fetch("http://localhost:8080/api/solutions", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ id, username, link }),
+        body: JSON.stringify({ id, username, link, description, title }),
       });
 
       if (response.ok) {
         const data = await response.json();
         console.log(data);
+        loadSolutions();
+        handleClose();
         // Handle success or display a success message
       } else {
         throw new Error("Request failed");
@@ -32,16 +36,22 @@ const SolutionForm = ({ id }) => {
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        ID:
-        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
-      </label>
-      <br />
-      <label>
         Username:
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Title:
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
         />
       </label>
       <br />
@@ -51,9 +61,19 @@ const SolutionForm = ({ id }) => {
           type="text"
           value={link}
           onChange={(e) => setLink(e.target.value)}
+          required
         />
       </label>
       <br />
+      <label>
+        Description:
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+      </label>
       <button type="submit">Submit</button>
     </form>
   );
---
+++ client/src/components/SolutionModal.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import SolutionForm from "./SolutionForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const SolutionModal = ({ loadSolutions, id }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        <FontAwesomeIcon icon={faPlus} /> Add Solution
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header className="TitleModal" closeButton>
+          <Modal.Title>Add Solution to Code Challenge</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SolutionForm
+            id={id}
+            handleClose={handleClose}
+            loadSolutions={loadSolutions}
+          />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default SolutionModal;
